8bc3dbaaaa3d3fb7400cb47bf59e027bfad95caf
rsjha945 <ravishankarjha7108gmail.com>
2022-07-06 21:54:24 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from typing import List
+
+# Declaring variables for setup functions
+PROJECT_NAME="housing_predictor"
+VERSION="0.0.1"
+AUTHOR="Ravi Shankar Jha"
+DESCRIPTION="This is a machine learning project"
+PACKAGES=["housing"]
+REQUIRED_FILE_NAME='requirements.txt'
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+
+    return: This function is going to return a list which contain name
+    of libraries mentioned in reuirements.txt file
+    """
+    with open(REQUIRED_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
